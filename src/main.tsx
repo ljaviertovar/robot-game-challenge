9adb022612fc86da9fb99fdbb57d1dada9a9c416
ljaviertovar <luisjavier_tovar@outlook.com>
2024-05-16 19:27:17 -0400

    update paths

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import App from './App'
 import '@/styles/globals.css'
-import { Toaster } from './components/ui/toaster.tsx'
+import { Toaster } from './components/ui/toaster'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
 	<React.StrictMode>
